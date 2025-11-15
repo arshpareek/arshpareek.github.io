82bbb5f8faf052ef033dc6628e1dffd8d09ae885
Arsh Pareek <arshdeep.pareek@gmail.com>
2025-11-15 10:10:57 +0530

    Fixed images

--- components/EducationPage.tsx
+++ components/EducationPage.tsx
@@ -16,7 +16,7 @@ export default function EducationPage() {
           university: "Imperial College London",
           degree: "Master of Science in Advanced Computing",
           date: "2021 - 2022",
-          logoURL: "https://yt3.googleusercontent.com/_gp1LqIURbX2pdr2bs64lMjrAhAzCauxVoEe31KXx24tABu9scVZKvFLue8ZrHlHWO3PmvLa52o=s900-c-k-c0x00ffffff-no-rj",
+          logoURL: "/icl.png",
           description: ["Achieved distinction classification for the thesis and merit classification for the entire degree.",
             "Topics covered: Mathematics for machine learning, natural language processing, robot learning, deep learning, computer vision, and computer security.",
           ]
@@ -25,7 +25,7 @@ export default function EducationPage() {
           university: "King's College London",
           degree: "Bachelor of Science in Computer Science",
           date: "2018 - 2021",
-          logoURL: "https://media.licdn.com/dms/image/v2/C560BAQEsapaVYleavg/company-logo_200_200/company-logo_200_200/0/1630592448557/kings_college_london_logo?e=2147483647&v=beta&t=Uo6FaZJkddeyOxm-oWR20SXIAQkE2LOhViU2ixvTtos",
+          logoURL: "/kcl.png",
           description: ["Achieved first class classification for the entire degree.",
             "Topics covered: Data structures, mathematical methods in physics, machine learning, and network security.",
           ],
@@ -34,7 +34,7 @@ export default function EducationPage() {
           university: "American School of Milan",
           degree: "International Baccalaureate Diploma",
           date: "2016 - 2018",
-          logoURL: "https://pbs.twimg.com/profile_images/1092718882194944000/EEa87Jt3_400x400.jpg",
+          logoURL: "/asm.png",
           description: ["Achieved a perfect grade of 45/45 (99.84 percentile) in International Baccalaureate Diploma with Physics, Chemistry and Maths at higher level and Economics, English and Italian at standard level.",
             "",
           ]
--- components/WorkPage.tsx
+++ components/WorkPage.tsx
@@ -15,7 +15,7 @@ export default function WorkPage() {
           company: "HCLTech",
           date: "Jun 2021 - Aug 2021",
           location: "London, United Kingdom",
-          logoURL: "https://media.licdn.com/dms/image/v2/C4D0BAQF-RIoeeMTMKQ/company-logo_100_100/company-logo_100_100/0/1664197008220/hcltech_logo?e=1762992000&v=beta&t=V01OKcqyIxg08EZ3z6a4LW0u2h63GALQCvbDlcbV5Es",
+          logoURL: "/hcl.png",
           description: "I worked as a machine learning intern at HCL Labs during the summer of 2021, where I implemented a tree-based classification model for detecting anomalous credit card use. The task involved working with Pandas and Numpy to clean and preprocess the training data and experimenting with unsupervised machine learning models to determine the most precise one. I then deployed my final implementation, which achieved a precision of over 99%, on the Google Cloud Platform."
         },
       ]
